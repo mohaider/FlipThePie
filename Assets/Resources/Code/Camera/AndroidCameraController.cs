bdd2d8958ae0e562f9c6398b0757406a5c369006
M. Haider <mrhaider@gmail.com>
2015-05-02 14:47:26 -0400

    Implemented a camera controller interface which will extend to other devices

---
+++ Assets/Resources/Code/Camera/AndroidCameraController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Resources.Code.Camera
+{
+    public class AndroidCameraController: MonoBehaviour, ICameraController
+    {
+        private WebCamTexture _cameraTexture;
+        private WebCamDevice[] _devices;
+        private string _deviceName;
+        public WebCamTexture CameraTexture
+        {
+            get
+            {
+                if (_cameraTexture == null)
+                {
+                    _cameraTexture = new WebCamTexture(DeviceName,400,300,15);
+                }
+                return _cameraTexture;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public WebCamDevice[] Devices
+        {
+            get
+            {
+                if (_devices == null)
+                {
+                    _devices = WebCamTexture.devices;
+                }
+                return _devices;
+            }
+
+        }
+
+        public string DeviceName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void TakePicture()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePicture()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    
+    }
+}
---
+++ Assets/Resources/Code/Camera/ICameraController.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Resources.Code.Camera
+{
+    /// <summary>
+    /// Interface for the camera controller
+    /// </summary>
+    public interface ICameraController
+    {
+        WebCamTexture CameraTexture
+        {
+            get; set; 
+        }
+
+        WebCamDevice[] Devices
+        {
+            get;
+        }
+
+        string DeviceName { get; set; }
+
+        void TakePicture();
+
+        void SavePicture();
+        
+
+    }
+}
